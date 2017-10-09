618f977e4c331270338d5e688b3933a5353d6a5e
DESKTOP-EHRVAU5\dso75 <dso753@naver.com>
2017-10-09 17:05:09 +0900

    위젯 추가(Button, CheckBox) 및 리스너 등록

--- app/src/main/java/com/bignerdranch/android/criminallntent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminallntent/CrimeFragment.java
@@ -8,12 +8,19 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.DateFormat;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
 
     private EditText mCrimeTitle;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,8 +33,12 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime, container, false);
 
+        //위젯 결합
         mCrimeTitle = (EditText) view.findViewById(R.id.crime_title);
+        mDateButton = (Button) view.findViewById(R.id.crime_date);
+        mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
 
+        //EditText 리스너
         mCrimeTitle.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -42,6 +53,18 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable editable) {
 
+            }
+        });
+
+        //date 버튼 default 값 지정 및 비활성화
+        mDateButton.setText(DateFormat.getDateInstance(java.text.DateFormat.FULL).format(mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        //CheckBox 리스터
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setSolved(mSolvedCheckBox.isChecked());
             }
         });
 
