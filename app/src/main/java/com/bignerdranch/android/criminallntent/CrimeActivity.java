e9cc9ad80a44bab97022762913608f6171d2342e
DESKTOP-EHRVAU5\dso75 <dso753@naver.com>
2017-10-09 22:48:56 +0900

    Intent 생성 메서드 추가, 프래그먼트 인자 가져오기

--- app/src/main/java/com/bignerdranch/android/criminallntent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminallntent/CrimeActivity.java
@@ -1,12 +1,26 @@
 package com.bignerdranch.android.criminallntent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+import java.util.UUID;
 
+public class CrimeActivity extends SingleFragmentActivity {
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminallntent.crime_id";    //다른 클래스에서 직접 액세스 안하기 때문에 private
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
